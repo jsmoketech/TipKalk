d514cb719e0c2503ef81d0c4a987ff483a2012a9
jSmokeTech <jsmoketech@gmail.com>
2019-09-06 20:20:41 -0700

    Initial Commit

---
+++ TipKalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipKalk
+//
+//  Created by Cow Juary on 9/6/19.
+//  Copyright © 2019 Cow Juary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TipKalkTests/TipKalkTests.swift
@@ -0,0 +1,34 @@
+//
+//  TipKalkTests.swift
+//  TipKalkTests
+//
+//  Created by Cow Juary on 9/6/19.
+//  Copyright © 2019 Cow Juary. All rights reserved.
+//
+
+import XCTest
+@testable import TipKalk
+
+class TipKalkTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
